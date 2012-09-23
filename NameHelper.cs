9761c000acc5f16408786d3da86489e12737bb50
Mike Mooney <mmooney@mmdbsolutions.com>
2012-09-23 14:18:34 -0400

    MMDB.Shared v2.0.0.8
    Adding NameHelper class

---
+++ NameHelper.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace MMDB.Shared
+{
+	public static class NameHelper
+	{
+		public static string GetFullName(string firstName, string lastName)
+		{
+			string returnValue;
+			if (!string.IsNullOrEmpty(firstName) && !string.IsNullOrEmpty(lastName))
+			{
+				returnValue = string.Format("{0} {1}", firstName, lastName);
+			}
+			else if (string.IsNullOrEmpty(firstName) && !string.IsNullOrEmpty(lastName))
+			{
+				returnValue = lastName;
+			}
+			else if (!string.IsNullOrEmpty(firstName) && string.IsNullOrEmpty(lastName))
+			{
+				returnValue = firstName;
+			}
+			else
+			{
+				returnValue = "";
+			}
+			return returnValue;
+		}
+	}
+}
