0782c676ec3db8609f73b1e63b48c8b769925c21
Mike Mooney <mmooney@mmdbsolutions.com>
2012-12-02 22:59:08 -0500

    MMDB.Shared v1.0.0.11
    Added TestDataHelper class

---
+++ TestDataHelper.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace MMDB.Shared
+{
+	public class TestDataHelper
+	{
+		private static Random _random;
+		public static Random Random
+		{
+			get
+			{
+				if (_random == null)
+				{
+					_random = new Random();
+				}
+				return _random;
+			}
+		}
+
+		public static int RandomInt()
+		{
+			return TestDataHelper.Random.Next();
+		}
+
+		public static decimal RandomDecimal()
+		{
+			return Convert.ToDecimal(TestDataHelper.Random.NextDouble());
+		}
+	}
+}
