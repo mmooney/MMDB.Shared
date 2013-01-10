d27c07f8431066fbc5f68cc7afa923d5b7fb2f32
Mike Mooney <mmooney@mmdbsolutions.com>
2013-01-10 15:52:24 -0500

    MMDB.Shared v2.0.0.14
    Added new method and some comments to StreamHelper

--- StreamHelper.cs
+++ StreamHelper.cs
@@ -8,6 +8,22 @@ namespace MMDB.Shared
 {
 	public static class StreamHelper
 	{
+		/// <summary>
+		/// Creates a new memory stream with the provided data.  The position will point to the end of the stream.  It is the caller's responsibility to dispose of the stream.
+		/// </summary>
+		/// <param name="data"></param>
+		/// <returns></returns>
+		public static MemoryStream CreateMemoryStream(string data)
+		{
+			MemoryStream stream = new MemoryStream(data.Length+10);
+			StreamWriter writer = new StreamWriter(stream);
+			writer.Write(data);
+			return stream;
+		}
+
+		/// <summary>
+		/// Writes reads all the data in a stream, from beginning to end. Restores teh stream's previous position before returning
+		/// </summary>
 		public static string ReadAll(Stream stream)
 		{
 			long oldPosition = stream.Position;
@@ -21,11 +37,16 @@ namespace MMDB.Shared
 			return returnValue;
 		}
 
-		public static void WriteAll(Stream stream, string data)
+
+		/// <summary>
+		/// Writes all of the provided data to the stream object.  The position is not overridden; it will write from the current position and the final position will be the end of the written data.
+		/// </summary>
+		public static Stream WriteAll(Stream stream, string data)
 		{
 			var writer = new StreamWriter(stream);
 			writer.Write(data);
 			writer.Flush();
+			return stream;
 		}
 	}
 }
