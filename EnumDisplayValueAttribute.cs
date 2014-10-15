3ba34651256ab26cad1c0d49b495b031825b6e49
Mike Mooney <mmooney@mmdbsolutions.com>
2014-10-15 11:29:22 -0400

    Removing old copyright notices, updating ncrunch

--- EnumDisplayValueAttribute.cs
+++ EnumDisplayValueAttribute.cs
@@ -1,15 +1,4 @@
-﻿/*
-	Copyright © 2009 Michael J. Mooney
-	All code contained herein is property of Michael J. Mooney (Developer).  
-	It is being provided to the Keystone Regional Volleyball Association (Institution)
-	as a non-exclusive license for the purposes of its volleyball event registration system.  
-	As owner, the Developer reserves the right to reuse this code for any future projects.  
-	As licensee, the Institution is granted the right to use and modify this code for exclusively 
-	its volleyball event registration system.  The Insitution nor any of its members may not reuse 
-	for any other purposes or redistribute this software to any other parties without the 
-	expressed written permission of the Developer.
-*/
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
